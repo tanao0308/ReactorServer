59d09f550af097cd8e92b6379154e5d5289dc6d1
tanao0308 <tanao030801@foxmail.com>
2024-05-28 19:49:12 +0800

    modified test/threadpool.cpp

--- test/threadpool.cpp
+++ test/threadpool.cpp
@@ -1,6 +1,14 @@
-#include "../ThreadPool.h"
 #include <iostream>
 #include <vector>
+#include "../ThreadPool.h"
+
+int do_some_calaulation(int i)
+{
+	std::cout<<"Task "<<i<<" is processing\n";
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout<<"Task "<<i<<" is done\n";
+	return i*i;
+}
 
 int main() {
     // 获取线程池的单例实例
@@ -9,21 +17,12 @@ int main() {
     // 提交一些任务到线程池
     std::vector<std::future<int>> results;
 
-    for (int i = 0; i < 8; ++i) {
-        results.emplace_back(
-            pool.submit([i] {
-                std::cout << "Task " << i << " is processing\n";
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "Task " << i << " is done\n";
-                return i * i;
-            })
-        );
-    }
+    for (int i = 0; i < 8; ++i)
+        results.emplace_back(pool.submit(do_some_calaulation, i));
 
     // 获取结果
-    for (auto && result : results) {
+    for (auto && result : results)
         std::cout << result.get() << std::endl;
-    }
 
     return 0;
 }
